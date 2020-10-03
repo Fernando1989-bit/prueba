6c705ac8fca66b87547beb1c7b5f4c12c8bdd5d4
Daniel <omoreno296@accitesz.com>
2020-10-03 11:36:44 -0500

    Actualización

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -23,8 +23,6 @@ public class Prueba {
 
     
     
-    
-    
     private static void metodoPrueba() {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
