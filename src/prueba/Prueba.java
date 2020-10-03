91a085a3e861e6c5e66b821796fadf74d4bd58ac
YhaLong <45216454+YhaLong@users.noreply.github.com>
2020-10-03 11:06:03 -0500

    prueba de commit actualizacion de proyecto

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -16,6 +16,7 @@ public class Prueba {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Esto es una prueba");
     }
     
 }
